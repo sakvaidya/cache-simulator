1975219db5666d051b5ab3a1cf9e678a833047e2
sakvaidya <saketvaidya007@gmail.com>
2026-04-14 22:32:48 +0530

    feat: implement RAND replacement policy

--- replacement.py
+++ replacement.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import List
+import random
 from cache import CacheBlock
 
 
@@ -8,20 +9,32 @@ class ReplacementPolicy(ABC):
 
     @abstractmethod
     def choose_victim(self, blocks: List[CacheBlock], set_index: int) -> int:
-        """Return the index of the block to evict."""
         pass
 
     @abstractmethod
     def on_access(self, set_index: int, block_index: int):
-        """Called on every cache access (hit or miss) for bookkeeping."""
         pass
 
     @abstractmethod
     def on_insert(self, set_index: int, block_index: int):
-        """Called when a new block is inserted into the cache."""
         pass
 
     @abstractmethod
     def reset(self):
-        """Reset internal policy state."""
+        pass
+
+
+class RANDPolicy(ReplacementPolicy):
+    """Random replacement: evict a randomly chosen block."""
+
+    def choose_victim(self, blocks: List[CacheBlock], set_index: int) -> int:
+        return random.randrange(len(blocks))
+
+    def on_access(self, set_index: int, block_index: int):
+        pass  # RAND needs no bookkeeping
+
+    def on_insert(self, set_index: int, block_index: int):
+        pass
+
+    def reset(self):
         pass
