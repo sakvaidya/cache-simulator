fa72994f82b07de6478f554ffc504d4c9bbf1328
sakvaidya <saketvaidya007@gmail.com>
2026-04-14 22:08:35 +0530

    feat: add base ReplacementPolicy abstract class

--- replacement.py
+++ replacement.py
@@ -1,38 +1,27 @@
 from abc import ABC, abstractmethod
 from typing import List
-import random
 from cache import CacheBlock
 
 
 class ReplacementPolicy(ABC):
+    """Abstract base class for all cache replacement policies."""
+
     @abstractmethod
     def choose_victim(self, blocks: List[CacheBlock], set_index: int) -> int:
+        """Return the index of the block to evict."""
         pass
 
     @abstractmethod
     def on_access(self, set_index: int, block_index: int):
+        """Called on every cache access (hit or miss) for bookkeeping."""
         pass
 
     @abstractmethod
     def on_insert(self, set_index: int, block_index: int):
+        """Called when a new block is inserted into the cache."""
         pass
 
     @abstractmethod
     def reset(self):
-        pass
-
-
-class RANDPolicy(ReplacementPolicy):
-    """Random replacement: evict a randomly chosen block."""
-
-    def choose_victim(self, blocks: List[CacheBlock], set_index: int) -> int:
-        return random.randrange(len(blocks))
-
-    def on_access(self, set_index: int, block_index: int):
-        pass  # RAND needs no bookkeeping
-
-    def on_insert(self, set_index: int, block_index: int):
-        pass
-
-    def reset(self):
+        """Reset internal policy state."""
         pass
